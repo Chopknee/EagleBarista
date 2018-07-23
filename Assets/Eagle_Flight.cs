933038d1745bb20886726f31ed86a6bbd4a103ce
unknown <chopknee@gmail.com>
2018-07-23 18:37:02 -0400

    First commit of existing work.

---
+++ Assets/Eagle_Flight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle_Flight : MonoBehaviour {
+    [SerializeField]
+    public Vector2 mouseOffset;
+
+    public float speed;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 direction = new Vector2();
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            direction = new Vector2(Input.GetAxis("Horizontal"), direction.y);
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            direction = new Vector2(direction.x, Input.GetAxis("Vertical"));
+        }
+
+        direction.Normalize();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction += new Vector2(0, 70);
+        }
+        //Debug.Log(direction);
+        direction = direction * speed;
+        gameObject.GetComponent<Rigidbody2D>().AddForce(direction);
+        //transform.position = new Vector3(transform.position.x + direction.x, transform.position.y + direction.y, transform.position.z);
+    }
+}
---
+++ Assets/Talons_Grabbing.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talons_Grabbing : MonoBehaviour {
+
+    public bool grabbed = false;
+    public GameObject grabbedObject = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (grabbed && Input.GetButtonDown("Fire2")) {
+            grabbed = false;
+            grabbedObject.transform.parent = null;
+            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        
+        if (Input.GetButton("Fire1") && !grabbed) {
+            Debug.Log("TRIGGERED!");
+            grabbed = true;
+            grabbedObject  = collision.gameObject;
+            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            grabbedObject.transform.parent = gameObject.transform;
+        }
+
+
+    }
+}
